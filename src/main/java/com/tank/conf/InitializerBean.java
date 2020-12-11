85a593e9d8c3e81d154daf8d6c21624d0ec5c4b2
fuchun198435163.com <tank198435@163.com>
2020-12-11 18:23:33 +0800

    [fuchun] #tech ok

--- src/main/java/com/tank/conf/InitializerBean.java
+++ src/main/java/com/tank/conf/InitializerBean.java
@@ -1,8 +1,6 @@
 package com.tank.conf;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.tank.annotation.DriverOnDb;
-import lombok.val;
+import com.tank.service.db.DataSourceRegisterPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -18,14 +16,8 @@ import javax.annotation.Resource;
 public class InitializerBean {
 
   @Bean
-  @DriverOnDb("com.mysql.cj.jdbc.Driver")
-  public DruidDataSource initDataSource() {
-    val dataSource = new DruidDataSource();
-    dataSource.setUrl(this.environment.getProperty("url"));
-    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    dataSource.setUsername(this.environment.getProperty("username"));
-    dataSource.setPassword(this.environment.getProperty("password"));
-    return dataSource;
+  public DataSourceRegisterPostProcessor initDataSourceRegisterPostProcessor() {
+    return new DataSourceRegisterPostProcessor();
   }
 
   @Resource
